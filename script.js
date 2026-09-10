2ca2245243510f4c158a4f4b70ccc1d78a1fdd6d
Lee Hao Quan <kxngmadx@gmail.com>
2026-09-10 23:31:13 +0800

    Added buttons and certain UI, howerver a bit buggy at the moment

--- script.js
+++ script.js
@@ -1,79 +1,62 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const choiceButtons = document.querySelectorAll('.human-choice');
+const verdictMsg = document.getElementById('verdict');
+
 function getComputerChoice() {
     const computerOptions = ['rock', 'paper', 'scissors'];
-
     let computerChoice = Math.floor(Math.random() * 3);
     return computerOptions[computerChoice];
 }
 
-function getHumanChoice() {
-    const humanChoices = ['rock', 'paper', 'scissors', 'quit']
-    let humanChoice = prompt('Whats your choice, rock paper or scissors?')
 
-    if(humanChoice == null){
+const humanChoices = ['rock', 'paper', 'scissors', 'quit'];
+
+let humanChoice = '';
+
+choiceButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+    humanChoice = e.target.value;
+
+    if (humanChoice == null) {
         humanChoice = 'quit';
     }
-
     if (!humanChoices.includes(humanChoice)) {
         console.log('Please choose either rock, paper or scissors');
-        return getHumanChoice()
     }
+    console.log(humanChoice)
 
-    return humanChoice;
-}
+    playRound(getComputerChoice(), humanChoice)
+});
+})
+
 
 function playRound(computerChoice, humanChoice) {
     if (computerChoice == humanChoice) {
         console.log('Tie!');
     }
-
     // possibilities for human losing
-    if (
-        (humanChoice == 'rock' && computerChoice == 'paper') ||
+    if ((humanChoice == 'rock' && computerChoice == 'paper') ||
         (humanChoice == 'scissors' && computerChoice == 'rock') ||
-        (humanChoice == 'paper' && computerChoice == 'scissors')
-    ) {
-        console.log(`
+        (humanChoice == 'paper' && computerChoice == 'scissors')) {
+        verdictMsg.textContent = `
             Your choice: ${humanChoice}
             Computer choice: ${computerChoice}
             You lose! ${computerChoice} beats ${humanChoice}
-        `)
-        computerScore++
+        `;
+        computerScore++;
     }
-
     // Possibilities for human winning
-    if (
-        (humanChoice == 'paper' && computerChoice == 'rock') ||
+    if ((humanChoice == 'paper' && computerChoice == 'rock') ||
         (humanChoice == 'rock' && computerChoice == 'scissors') ||
-        (humanChoice == 'scissors' && computerChoice == 'paper')
-    ) {
-        console.log(`
+        (humanChoice == 'scissors' && computerChoice == 'paper')) {
+        verdictMsg.textContent = `
             Your choice: ${humanChoice}
             Computer choice: ${computerChoice}
             You win! ${humanChoice} beats ${computerChoice}
-        `)
-        humanScore++
+        `;
+        humanScore++;
     }
-
     console.log('Human Score: ' + humanScore, '\nComputer Score: ' + computerScore);
 }
-
-function main() {
-    let running = true;
-
-    while (running) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        if (humanSelection == 'quit'){
-            running = false;
-            break;
-        }
-
-        playRound(computerSelection, humanSelection);
-    }
-}
-
-main();
